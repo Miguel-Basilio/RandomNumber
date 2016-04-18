0a686fd8f3d1d9d82fc11d5ae1ece8182a9792a7
MiguelBasilio <lokoa@DESKTOP-F4BVPJT>
2016-04-17 23:28:06 -0700

    RandomNumberweek9

---
+++ src/RandomNumber.java
@@ -0,0 +1,14 @@
+
+public class RandomNumber {
+public int GetANumber (int high){
+	
+	int computerNumber = 1 +(int)(Math.random()* high);
+return computerNumber;
+}
+
+public int GetNumber_Between_1_and_10(){
+	int computerNumber = 1+ (int)(Math.random()*10);
+	return computerNumber;
+	
+}
+}
